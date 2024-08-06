6e5991bfd91c77cbc9c92a9de020edaf8bdea838
Alexander499 <aginfo9@gruenefeld.de>
2024-08-07 00:47:40 +0200

    Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
 function onSignIn(googleUser) {
-  // Get profile information
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-
-  // Get the Google ID token
-  var id_token = googleUser.getAuthResponse().id_token;
-
-  // Send the ID token to your server or verify it on the client side
-  // Example: sendIdTokenToServer(id_token);
+    // Get the Google user profile
+    var profile = googleUser.getBasicProfile();
+    
+    // Get ID token
+    var id_token = googleUser.getAuthResponse().id_token;
+    
+    // Retrieve user details
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    
+    // Proceed to store or use the information
+    // Example: sendIdTokenToServer(id_token);
 }
+
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
