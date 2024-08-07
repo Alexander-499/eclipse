c55e8a8805efa22676319c105df8f606891e5524
Alexander499 <aginfo9@gruenefeld.de>
2024-08-07 18:26:51 +0200

    Commit

--- lists/flyout-content.js
+++ lists/flyout-content.js
@@ -1,47 +1,237 @@
 flyoutContent =
 {
-    "targetSelectors": {
-        "target": {
-            "header": "Target",
-            "text": "",
-            "link": "https://minecraft.wiki/w/Target_selectors"
-        },
-        "limit": {
-            "header": "Limit",
-            "text": "",
-            "link": "https://minecraft.wiki/w/Target_selectors"
-        },
-        "sort": {
-            "header": "Sort",
-            "text": "",
-            "link": "https://minecraft.wiki/w/Target_selectors"
-        },
-        "x": {
-            "header": "X",
-            "text": "",
-            "link": "https://minecraft.wiki/w/Target_selectors"
-        },
-        "y": {
-            "header": "Y",
-            "text": "",
-            "link": "https://minecraft.wiki/w/Target_selectors"
-        },
-        "z": {
-            "header": "Z",
-            "text": "",
-            "link": "https://minecraft.wiki/w/Target_selectors"
-        }
+  "targetSelectors": {
+    "target": {
+      "header": "Target",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
     },
-    "item": {
-        "item": {
-            "header": "Item",
-            "text": "",
-            "link": "https://minecraft.wiki/w/Items"
-        },
-        "count": {
-            "header": "Count",
-            "text": "",
-            "link": "https://minecraft.wiki/w/NBT_format"
-        }
+    "limit": {
+      "header": "Limit",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "sort": {
+      "header": "Sort",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "x": {
+      "header": "X",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "y": {
+      "header": "Y",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "z": {
+      "header": "Z",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "selectionArea": {
+      "header": "Selection Area",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "minVertRotation": {
+      "header": "Min Vert Rotation",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "maxVertRotation": {
+      "header": "Max Vert Rotation",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "minHorizRotation": {
+      "header": "Min Horiz Rotation",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "maxHorizRotation": {
+      "header": "Max Horiz Rotation",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "team": {
+      "header": "Team",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "name": {
+      "header": "Name",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "nbt": {
+      "header": "NBT",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "gameMode": {
+      "header": "Game Mode",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "minXpLevel": {
+      "header": "Min XP Level",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "maxXpLevel": {
+      "header": "Max XP Level",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "advancements": {
+      "header": "Advancements",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "tags": {
+      "header": "Tags",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "predicates": {
+      "header": "Predicates",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
+    },
+    "scoreObjectives": {
+      "header": "Score Objectives",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Target_selectors"
     }
+  },
+  "item": {
+    "item": {
+      "header": "Item",
+      "text": "",
+      "link": "https://minecraft.wiki/w/Items"
+    },
+    "count": {
+      "header": "Count",
+      "text": "",
+      "link": "https://minecraft.wiki/w/NBT_format"
+    },
+    "maxStackSize": {
+      "header": "Max Stack Size",
+      "text": "",
+      "link": ""
+    },
+    "customName": {
+      "header": "Custom Name",
+      "text": "",
+      "link": ""
+    },
+    "itemName": {
+      "header": "Item Name",
+      "text": "",
+      "link": ""
+    },
+    "loreName": {
+      "header": "Lore Name",
+      "text": "",
+      "link": ""
+    },
+    "maxDamage": {
+      "header": "Max Damage",
+      "text": "",
+      "link": ""
+    },
+    "damage": {
+      "header": "Damage",
+      "text": "",
+      "link": ""
+    },
+    "repairCost": {
+      "header": "Repair Cost",
+      "text": "",
+      "link": ""
+    },
+    "unbreakable": {
+      "header": "Unbreakable",
+      "text": "",
+      "link": ""
+    },
+    "showInTooltip": {
+      "header": "Show In Tooltip",
+      "text": "",
+      "link": ""
+    },
+    "fireResistant": {
+      "header": "Fire Resistant",
+      "text": "",
+      "link": ""
+    },
+    "rarity": {
+      "header": "Rarity",
+      "text": "",
+      "link": ""
+    },
+    "customModelData": {
+      "header": "Custom Model Data",
+      "text": "",
+      "link": ""
+    },
+    "customData": {
+      "header": "Custom Data",
+      "text": "",
+      "link": ""
+    },
+    "hideTooltip": {
+      "header": "Hide Tooltip",
+      "text": "",
+      "link": ""
+    },
+    "hideAdditionalTooltip": {
+      "header": "Hide Additional Tooltip",
+      "text": "",
+      "link": ""
+    },
+    "canPlaceOn": {
+      "header": "Can Place On",
+      "text": "",
+      "link": ""
+    },
+    "canBreak": {
+      "header": "Can Break",
+      "text": "",
+      "link": ""
+    },
+    "enchantments": {
+      "header": "Enchantments",
+      "text": "",
+      "link": ""
+    },
+    "food": {
+      "header": "Food",
+      "text": "",
+      "link": ""
+    },
+    "jukeboxPlayable": {
+      "header": "Jukebox Playable",
+      "text": "",
+      "link": ""
+    },
+    "tool": {
+      "header": "Tool",
+      "text": "",
+      "link": ""
+    },
+    "attributeModifiers": {
+      "header": "Attribute Modifiers",
+      "text": "",
+      "link": ""
+    },
+    "blockState": {
+      "header": "Block State",
+      "text": "",
+      "link": ""
+    }
+  }
 };
