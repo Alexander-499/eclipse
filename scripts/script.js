4ace12c6ec8d02028f74960df113c4430e3827c1
Alexander499 <aginfo9@gruenefeld.de>
2024-08-07 15:38:14 +0200

    Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
-function onSignIn(googleUser) {
-    // Get the Google user profile
-    var profile = googleUser.getBasicProfile();
+// function onSignIn(googleUser) {
+//     // Get the Google user profile
+//     var profile = googleUser.getBasicProfile();
     
-    // Get ID token
-    var id_token = googleUser.getAuthResponse().id_token;
+//     // Get ID token
+//     var id_token = googleUser.getAuthResponse().id_token;
     
-    // Retrieve user details
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+//     // Retrieve user details
+//     console.log('ID: ' + profile.getId());
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail());
     
-    // Proceed to store or use the information
-    // Example: sendIdTokenToServer(id_token);
-}
+//     // Proceed to store or use the information
+//     // Example: sendIdTokenToServer(id_token);
+// }
 
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-      console.log('User signed out.');
-  });
-}
+// function signOut() {
+//   var auth2 = gapi.auth2.getAuthInstance();
+//   auth2.signOut().then(function () {
+//       console.log('User signed out.');
+//   });
+// }
